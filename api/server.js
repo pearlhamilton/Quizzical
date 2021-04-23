78b9fb575e0cb2dca6ea588dc54a35e2d1a46d7c
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-23 20:32:00 +0100

    add api welcome message

--- api/server.js
+++ api/server.js
@@ -4,3 +4,9 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+
+// Root route
+server.get('/', (req, res) => res.send("Hello, world! Welcome to Quizzical\'s API!"))
+
+module.exports = server
