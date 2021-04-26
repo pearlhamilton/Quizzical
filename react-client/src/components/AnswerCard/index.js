2aad805267ed2e03ef3f9cdcf909e98d0222c553
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-26 10:13:52 +0100

    create Quiz page and answer card component

---
+++ react-client/src/components/AnswerCard/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const AnswerCard = () => {
+    return (
+        <h1> I am an answer card </h1>
+    )
+}
+
+export default AnswerCard;
---
+++ react-client/src/pages/Quiz/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Quiz = () => {
+    return(
+        <h1> I am the quiz page</h1>
+    )
+}
+
+export default Quiz
