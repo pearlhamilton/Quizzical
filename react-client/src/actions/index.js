19e281184bbdb2e086053bc26013153acdcde5b5
sem-the-dev <semhar.tesfu@outlook.com>
2021-04-27 18:54:08 +0100

    add category input

--- react-client/src/actions/index.js
+++ react-client/src/actions/index.js
@@ -1,14 +1,10 @@
-// write redux actions here
-// e.g.
-// export const addUser = newUser => ({ type: 'ADD_USER', payload: newUser })
-
 import axios from "axios";
 
-export const fetchQuiz = (difficulty, numberOfQs) => {
+export const fetchQuiz = (numberOfQs, subject, difficulty) => {
   return async (dispatch) => {
     try {
       const { data } = await axios.get(
-        `https://opentdb.com/api.php?amount=${numberOfQs}&category=9&difficulty=${difficulty}&type=multiple`
+        `https://opentdb.com/api.php?amount=${numberOfQs}&category=${subject}&difficulty=${difficulty}&type=multiple`
       );
 
       let quizData = data.results.map((element, i) => ({
@@ -17,18 +13,7 @@ export const fetchQuiz = (difficulty, numberOfQs) => {
         correct_answer: element.correct_answer,
         answers: [...element.incorrect_answers, element.correct_answer],
       }));
-      console.log(quizData);
-      // console.log(quizData[0].correct_answer);
-      // console.log(quizData[1].correct_answer);
-      // console.log(quizData[2].correct_answer);
-      // console.log(quizData[3].correct_answer);
-      // console.log(quizData[4].correct_answer);
-      // console.log(quizData[5].correct_answer);
-      // console.log(quizData[6].correct_answer);
-      // console.log(quizData[7].correct_answer);
-      // console.log(quizData[8].correct_answer);
-      // console.log(quizData[9].correct_answer);
-
+     
       dispatch({
         type: "LOAD_QUESTIONS",
         payload: quizData,
--- react-client/src/pages/Home/index.js
+++ react-client/src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { fetchCategory } from "../../actions";
+
 import { Form } from "../../components";
 
 function Home() {
   return (
     <div>
-      <button onClick={fetchCategory}>get catgories</button>
+    
       <Form />
     </div>
   );
