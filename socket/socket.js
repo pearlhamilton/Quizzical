150c1771bab4625aa49b706adf7958b79dcb94bd
gretaivan <greta.ivan91@gmail.com>
2021-04-27 21:00:31 +0100

    separates socket and index files

--- socket/socket.js
+++ socket/socket.js
@@ -12,91 +12,6 @@ const io = require("socket.io")(server, {
     }
 });
 
-// io.on('connection', socket => {
-//     console.log("'Ello, who's this we got here?")
-
-//     // get total number of connections
-//     const participantCount = io.engine.clientsCount
-//     // send event only to connecting User
-//     socket.emit('admin-message', 'Hi there, new friend!')
-//     // send event to all others Users 
-//     socket.broadcast.emit('admin-message', `A new friend has arrived!`)
-//     // send event to all Users
-//     io.emit('admin-message', `There is ${participantCount} x friend here now!`)
-
-//     // *************************************************************************************
-//     // HANDLE USER ENTERS ROOM
-
-//     socket.on('request-join-game', ({gameId, username}) => { // needs gameId from client
-//         console.log(`${username} joining ${gameId}`)
-
-//         socket.join(gameId) // add User to room (creates new room if no matching room ID)
-//         // send event to all other Users in specific room
-//         socket.broadcast.to(gameId).emit('add-user', { username })
-
-//         // gather updated room data
-//         const roomData = io.sockets.adapter.rooms[gameId]
-//         const inRoomCount = roomData.length
-//         const inRoomIds = Object.keys(roomData.sockets)
-
-//         // send event only to User
-//         socket.emit('entry-permission', { gameId, players: inRoomIds})
-//         // send event to all other Users in specific room
-//         socket.to(gameId).emit('new-player-joining', { username, gameId })
-//         // send event to all Users in specific room
-//         io.in(gameId).emit('admin-message', `${inRoomCount} players now in ${gameId}!`)
-//     })
-
-//     // *************************************************************************************
-//     // HANDLE USER GAME ACTION
-//     // ('answer-question', currentQuestion, success, username, current.gameId )
-//     socket.on('answer-question', ({question, success, username, gameId}) => {
-//         const message = `${username} answered "${question.text}" ${success ? 'correctly!' : 'incorrectly :('}`
-//         console.log(message)
-//         socket.broadcast.to(gameId).emit('remote-question-answered', { question, message })
-//     })
-
-//     socket.on('user-completed', ({ username, gameId, points }) => {
-//         socket.broadcast.to(gameId).emit('remote-user-completed', { username, points })
-//     })
-
-//     // *************************************************************************************
-//     // HANDLE USER EXITS ROOM
-
-//     socket.on('leave-game', ({username, gameId}) => {
-//         socket.leave(gameId)
-//         const roomData = io.sockets.adapter.rooms[gameId]
-//         const inRoomCount = roomData.length
-//         io.in(gameId).emit('admin-message', `${username} has left the game! ${inRoomCount} players remain in ${gameId}...`)
-//     })
-
-//     // *************************************************************************************
-//     // HANDLE USER EXITS APP / DISCONNECTS
-
-
-//     socket.on("disconnect", socket => {
-//         console.log("K bye then");
-//     });
-// });
-
-// const port = process.env.PORT || 5001;
-
-// server.listen(port, () => console.log(`Open for play on port ${port}!`));
-
-
-// io.on('connection', client => {
-//   client.on('event', data => { /* … */ });
-//   client.on('disconnect', () => { /* … */ });
-// });
-
-
-const port = process.env.PORT || 5001;
-
-server.listen(port, () => console.log(`Socket is on ${port}!`))
-
-
-
-
 //io overall session || socket sesion branches
 io.on('connection', socket => {
 
@@ -197,3 +112,83 @@ io.on('connection', socket => {
         
     });
 });
+
+
+// io.on('connection', socket => {
+//     console.log("'Ello, who's this we got here?")
+
+//     // get total number of connections
+//     const participantCount = io.engine.clientsCount
+//     // send event only to connecting User
+//     socket.emit('admin-message', 'Hi there, new friend!')
+//     // send event to all others Users 
+//     socket.broadcast.emit('admin-message', `A new friend has arrived!`)
+//     // send event to all Users
+//     io.emit('admin-message', `There is ${participantCount} x friend here now!`)
+
+//     // *************************************************************************************
+//     // HANDLE USER ENTERS ROOM
+
+//     socket.on('request-join-game', ({gameId, username}) => { // needs gameId from client
+//         console.log(`${username} joining ${gameId}`)
+
+//         socket.join(gameId) // add User to room (creates new room if no matching room ID)
+//         // send event to all other Users in specific room
+//         socket.broadcast.to(gameId).emit('add-user', { username })
+
+//         // gather updated room data
+//         const roomData = io.sockets.adapter.rooms[gameId]
+//         const inRoomCount = roomData.length
+//         const inRoomIds = Object.keys(roomData.sockets)
+
+//         // send event only to User
+//         socket.emit('entry-permission', { gameId, players: inRoomIds})
+//         // send event to all other Users in specific room
+//         socket.to(gameId).emit('new-player-joining', { username, gameId })
+//         // send event to all Users in specific room
+//         io.in(gameId).emit('admin-message', `${inRoomCount} players now in ${gameId}!`)
+//     })
+
+//     // *************************************************************************************
+//     // HANDLE USER GAME ACTION
+//     // ('answer-question', currentQuestion, success, username, current.gameId )
+//     socket.on('answer-question', ({question, success, username, gameId}) => {
+//         const message = `${username} answered "${question.text}" ${success ? 'correctly!' : 'incorrectly :('}`
+//         console.log(message)
+//         socket.broadcast.to(gameId).emit('remote-question-answered', { question, message })
+//     })
+
+//     socket.on('user-completed', ({ username, gameId, points }) => {
+//         socket.broadcast.to(gameId).emit('remote-user-completed', { username, points })
+//     })
+
+//     // *************************************************************************************
+//     // HANDLE USER EXITS ROOM
+
+//     socket.on('leave-game', ({username, gameId}) => {
+//         socket.leave(gameId)
+//         const roomData = io.sockets.adapter.rooms[gameId]
+//         const inRoomCount = roomData.length
+//         io.in(gameId).emit('admin-message', `${username} has left the game! ${inRoomCount} players remain in ${gameId}...`)
+//     })
+
+//     // *************************************************************************************
+//     // HANDLE USER EXITS APP / DISCONNECTS
+
+
+//     socket.on("disconnect", socket => {
+//         console.log("K bye then");
+//     });
+// });
+
+// const port = process.env.PORT || 5001;
+
+// server.listen(port, () => console.log(`Open for play on port ${port}!`));
+
+
+// io.on('connection', client => {
+//   client.on('event', data => { /* … */ });
+//   client.on('disconnect', () => { /* … */ });
+// });
+
+module.exports = server;
