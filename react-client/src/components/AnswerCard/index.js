f7859eeafc66f7c7253f11bbd3f82e36ca775428
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-28 09:04:04 +0100

    change hardcorded 9 to arraylength-1

--- react-client/src/components/AnswerCard/index.js
+++ react-client/src/components/AnswerCard/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { endQuestions, changeQuestion } from "../../actions";
+import {useSelector} from "react-redux"
 
 const AnswerCard = (props) => {
 
   const dispatch = useDispatch();
+  
   const nextQuestion = (answer) => dispatch(changeQuestion(answer));
   const endQuestion = (finalAnswer) => dispatch(endQuestions(finalAnswer))
+  const results = useSelector((state) => state.results)
+  const questionArrayLength = results.length
 
     // index 9 is where the code broke, so if the answer that is clicked on is at index 9 of the array, do not go to the next question, instead end the questions
 
-  if (props.index === 9){
+  if (props.index === questionArrayLength-1){
     return(
         <button style={{color: "white", backgroundColor: "pink"}} onClick = { () => endQuestion(props.answer)}>{props.answer}</button>
     )
