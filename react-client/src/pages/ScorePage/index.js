03bde774f536c69145a3af4610b8ddfd70d003de
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-29 19:41:55 +0100

    paste in working file

--- react-client/src/pages/ScorePage/index.js
+++ react-client/src/pages/ScorePage/index.js
@@ -6,21 +6,16 @@ import axios from 'axios'
 import {socket} from '../../socket/index.js';
 
 const ScorePage =() => {
-
 const username = useSelector((state) => state.user.user.username)
-
 const score = useSelector((state)=> state.quizReducer.score) //get score from state
 
-
-  const getPlayers = () => {
-
+  
+const getPlayers = () => {
     //TODO: extract from redux room name and store to roomName
-    
     socket.on('get-player-data', roomName, (res) => {
       console.log(res)
     }); 
   }
-
 
   const sendResults = () => {
     return new Promise(async (resolve, reject) => {
@@ -44,23 +39,16 @@ const score = useSelector((state)=> state.quizReducer.score) //get score from st
         }
     })
   }
-
     useEffect(() => {
         sendResults()
-
     })
-
   return (
-    <>
-    {/* <div id="score-page">
-    <div id="playerscore"> */}
-      <h1 role="heading">You scored: {score}</h1>
-
-    
+    <div id="score-page">
+    <div id="playerscore">
+      <h1>You scored: {score}</h1>
     <Link to="/leaderboard"><button>Go to Leaderboard</button></Link>
-    {/* </div>
-    </div> */}
-    </>
+    </div>
+    </div>
   );
 }
 export default ScorePage;
