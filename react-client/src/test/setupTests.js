546e8ea372a555023afd3647b4f1862ea6b8c85c
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-29 17:56:28 +0100

    paste test file

--- react-client/src/test/setupTests.js
+++ react-client/src/test/setupTests.js
@@ -1,34 +1,37 @@
 import React from "react";
-
 import "@testing-library/jest-dom";
-
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-
-import quizReducer from "../reducers/quizReducer";
-
-const TestProviders = ({ initState }) => {
+import {quizReducer, userReducer} from "../reducers";
+const TestProviders = ({ initState, defaultState }) => {
   initState ||= {
     location: "",
-    results: [{ question: "", correct_answer: "", answers: [] }],
+    results: [{ question: "Hello", correct_answer: "hello", answers: ["hello"] }],
     current_question_index: 0,
     score: 0,
     endOfQuestions: false,
     loading: false,
   };
+  defaultState ||= {
+    user: { username: "testUser", type: "HOST" },
+    id: "adsfasfafsaf",
+    room: "hello"
+  };
   let testReducer = () => quizReducer(initState, { type: "@@INIT" });
-  const testStore = createStore(testReducer, applyMiddleware(thunk));
-
+  let testReducerTwo = () => userReducer(defaultState, { type: "@@INIT"})
+  const rootReducer = combineReducers({
+    quizReducer:testReducer,
+    user: testReducerTwo
+  })
+  const testStore = createStore(rootReducer, applyMiddleware(thunk));
   return ({ children }) => <Provider store={testStore}>{children}</Provider>;
 };
-
 const renderWithReduxProvider = (ui, options = {}) => {
   let TestWrapper = TestProviders(options);
   render(ui, { wrapper: TestWrapper, ...options });
 };
-
 import axios from "axios";
 jest.mock("axios");
 axios.get.mockResolvedValue({
@@ -39,6 +42,5 @@ axios.get.mockResolvedValue({
     },
   ],
 });
-
 global.renderWithReduxProvider = renderWithReduxProvider;
 global.React = React;
