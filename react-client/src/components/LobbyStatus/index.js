bf95f9dd390ef6c64db6e400b9a6467869ca1ffb
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-04-29 11:34:34 +0100

    get chosen quiz values from reducer and display to players

--- react-client/src/components/LobbyStatus/index.js
+++ react-client/src/components/LobbyStatus/index.js
@@ -4,6 +4,11 @@ import {useSelector} from "react-redux"
 
 const LobbyStatus = () => {
 
+
+    const subject = useSelector((state) => state.config.config.subject) //need to change this back into words
+    const difficulty = useSelector((state) => state.config.config.difficulty)
+    const numberOfQs = useSelector((state) => state.config.config.numberOfQs ) 
+
     // const usertype = useSelector((state) => state.user.user.type)
     const usertype = "PLAYER"
 
@@ -17,8 +22,11 @@ const LobbyStatus = () => {
     }
     else{
         return(
+            <>
         <p>Waiting for host to start the game</p>
         
+        <p>You will be answering {numberOfQs} questions about {subject} at difficulty level{difficulty}</p>
+        </>
         )
     }
 
