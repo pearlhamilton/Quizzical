df24da2d110a8d8d25fa2d1d25e75d617254578f
sem-the-dev <semhar.tesfu@outlook.com>
2021-04-29 18:34:27 +0100

    tests

--- react-client/src/components/Form/Form.test.js
+++ react-client/src/components/Form/Form.test.js
@@ -1,5 +1,5 @@
 import { default as Form } from ".";
-import { screen } from "@testing-library/react";
+import { screen, render  } from "@testing-library/react";
 // import userEvent from "@testing-library/user-event";
 // import { MemoryRouter } from "react-router-dom";
 
@@ -10,7 +10,8 @@ describe("Form", () => {
   });
 
   test("it renders a form", () => {
-    let form = screen.getByRole("form");
+    let form = screen.getByLabelText("game-selection");
+    //const correctAnswer = state.results[0].correct_answer;
     expect(form).toBeInTheDocument();
   });
 
@@ -19,14 +20,17 @@ describe("Form", () => {
     expect(categoryInput).toBeInTheDocument();
   });
 
-  //   test("it calls category choice on form submission", () => {
-  //     let categoryInput = screen.getByLabelText("category");
-  //     userEvent.type(categoryInput, "Music{enter}");
-  //     expect(getResultMock).toHaveBeenNthCalledWith(1, "Music");
+  // test("it calls category choice on form submission", () => {
+  //   let categoryInput = screen.getByRole("category", {
+  //     name: /Pick a category/i
   //   });
+  //   //userEvent.type(categoryInput, "Music{enter}");
+  //   expect(categoryInput).toBeInTheDocument();
+  //   //expect(getResultMock).toHaveBeenNthCalledWith(1, "Music");
+  // });
 
   test("it renders a number of questions option", () => {
-    let numberOfQuestionsInput = screen.getByLabelText("number of questions");
+    let numberOfQuestionsInput = screen.getByAltText("number of questions");
     expect(numberOfQuestionsInput).toBeInTheDocument();
   });
 
